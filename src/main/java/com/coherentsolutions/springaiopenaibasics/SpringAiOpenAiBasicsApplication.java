38982df5246c56e86e58cafaaeaf8f327478f174
iWaraxe <igor.waraxe@gmail.com>
2025-04-13 22:41:50 +0200

    Initial commit

---
+++ src/main/java/com/coherentsolutions/springaiopenaibasics/SpringAiOpenAiBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.springaiopenaibasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOpenAiBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOpenAiBasicsApplication.class, args);
+	}
+
+}
